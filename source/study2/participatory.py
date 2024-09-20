8445e10d3097cdee853d16b1f29c5e56f36c9119
gizemsogancioglu <gizemsogancioglu@gmail.com>
2024-09-20 15:06:52 +0200

    participatory fix

--- source/study2/participatory.py
+++ source/study2/participatory.py
@@ -3,100 +3,39 @@ from scipy.stats import ttest_rel
 import numpy as np
 import collections
 import scipy.stats as stats
-from sklearn.metrics import recall_score, f1_score, confusion_matrix, \
-    roc_auc_score
 import warnings
 warnings.filterwarnings('ignore')
-from eval import eval, save_res
-
-from scipy.stats import shapiro
-
-def bootstrap_test_model_comparison(model1_scores, model2_scores, n_bootstrap=10000):
-    observed_diff = np.mean(model2_scores) - np.mean(model1_scores)
-    combined_scores = np.concatenate([model1_scores, model2_scores])
-    count_extreme = 0
-                    
-    for _ in range(n_bootstrap):
-        bootstrap_sample = np.random.choice(combined_scores, size=len(combined_scores), replace=True)
-        bootstrap_diff = np.mean(bootstrap_sample[len(model1_scores):]) - np.mean(bootstrap_sample[:len(model1_scores)])
-                                                    
-        if bootstrap_diff >= observed_diff:
-            count_extreme += 1
-                                                                                        
-    p_value = count_extreme / n_bootstrap
-    return p_value
+from eval import save_res
 
 def compare_df_values_multi(df_list, df_names, column_name, embedding, fold):
-    # Assuming all dataframes are of the same length and have aligned indices
-        # Extract the value from each dataframe for the current row and column
     values = [df[df['embedding'] == embedding].mean()[column_name] for df in df_list]
     max_value = max(values)
     max_index = values.index(max_value)
-    #pint(f"Row : {df_names[max_index]} has the higher value of {max_value}")
+    print(f"Row : {df_names[max_index]} has the higher value of {max_value}")
 
-    #t_stat, p_value = stats.ttest_rel(df_list[0][column_name], df_list[1][column_name])
-    #if df_names[max_index] == 'neutr':
-        #pred_orig = pd.read_csv("../preds/predictions_MIMIC_DEPRESSION_majority_orig_fold{num}_{emb}.csv".format(num=fold,emb=embedding))
-        #pred_neutr = pd.read_csv("../preds/predictions_MIMIC_DEPRESSION_majority_neutr_fold{num}_{emb}.csv".format(num=fold,emb=embedding))
-        #p_value = bootstrap_test_model_comparison(pred_orig, pred_neutr, n_bootstrap=100)
-        #p_value = bootstrap_test_model_comparison(df_list[0][column_name], df_list[1][column_name], n_bootstrap=10000)
-    
-    # Calculate the differences
-    differences = df_list[0][column_name] - df_list[1][column_name]
-
-    # Perform the Shapiro-Wilk test for normality
-    stat, p = shapiro(differences)
-
-    # Interpret the results
-    #alpha = 0.05
-    #if p > alpha:
-     #   print('The difference between the models seems to be normally distributed (fail to reject H0)')
-    #else:
-     #   print('The difference between the models does not appear to be normally distributed (reject H0)')
-   
     __, p_value = ttest_rel(df_list[0][column_name], df_list[max_index][column_name])
-    #p_value, tstat = two_sample_paired_sign_test(df_list[0][column_name], df_list[max_index][column_name])
-    #print("p-val", p_value)
-    #p_value = p_value/2
-    #print("p-val from paired t-test: ", p_value, tstat)
     if p_value > 0.05:
-        #print("not significant")
+        print("not significant")
         return "orig"
     else:
-        #print("WOW")
+        print("significant")
         return df_names[max_index]
 
     return  df_names[max_index]
 
-def two_sample_paired_sign_test(S1, S2, alternative='greater'):
-        
-    S1 = np.array(S1)
-    S2 = np.array(S2)
-    m = len(S1)
-    assert m > 0
-    assert len(S2) == m
-    assert alternative in ('greater', 'less')
-    test_stat = np.greater_equal(S1 - S2, 0.0).sum()
-    p_value = stats.binom_test(test_stat, n=len(S1), p=0.5, alternative=alternative)
-
-    return p_value, test_stat
-
 def participatory_method(config, dataset, measure):
-    #method_arr = ['orig', 'neutr', 'augmented', 'gender_specific']
     method_arr = ['orig', 'neutr', 'augmented', 'gender_specific']
     scores_arr = collections.defaultdict(list)
     for embedding in embeddings:
         df_list = []
         for method in method_arr:
-            data = pd.read_csv('../results/{dataset}/{measure}/val_experiments_{dataset}_{m}.csv'.format(dataset=dataset, m=method, measure=measure.lower()))
+            data = pd.read_csv(f'../results/val_experiments_{dataset}_{method}.csv')
             #data = data[data['embedding'] == embedding].mean()
             df_list.append(data)
-        #print(data)
         # df_list.append(data[(data['embedding'] == embedding) & (data['fold_num'] == fold)])
         for fold in range(config):
             preds = pd.read_csv(
-                "../preds/{dataset}/{measure}/predictions_{dataset}_orig_fold{num}_{emb}.csv".format(dataset=dataset, num=fold,measure=measure.lower(),
-                                                                                                    emb=embedding))
+                f"../preds/predictions_{dataset}_orig_fold{fold}_{embedding}.csv")
             predictions = pd.Series(index=preds.index, dtype='float64')
             predictions_prob = pd.Series(index=preds.index, dtype='float64')
 
@@ -126,22 +65,20 @@ def participatory_method(config, dataset, measure):
                               best_preds[[clf, 'GENDER']].reset_index(drop=True)], axis=1)
 
             data.to_csv(
-                "../preds/{dataset}/{measure}/predictions_{dataset}_{best}_fold{num}_{emb}.csv".format(dataset=dataset, clf=clf, best='participatory',
+                "../preds/predictions_{dataset}_{best}_fold{num}_{emb}.csv".format(dataset=dataset, clf=clf, best='participatory',
                                                                                         num=fold, emb=embedding, measure=measure.lower()))
 if __name__ == "__main__":
-
-    #dataset = 'MIMIC'
-    dataset = 'UMC'
-
+    dataset = 'MIMIC'
+    #dataset = 'UMC'
     gender_dict = {"UMC": [0, 1], "MIMIC": ['F','M']}
     attr = 'GENDER'
 
     if dataset == 'MIMIC':
-        config = 50
+        config = 10
         clf = 'DEPRESSION_majority'
         label  = 'DEPRESSION_majority'
-        embeddings = ['w2vec_news', 'biowordvec', 'bert', 'clinical_bert']
-        #embeddings = ['w2vec_news']
+        #embeddings = ['w2vec_news', 'biowordvec', 'bert', 'clinical_bert']
+        embeddings = ['w2vec_news']
         measure = 'F1'
 
     else: 
@@ -162,21 +99,17 @@ if __name__ == "__main__":
         for embedding in embeddings:
             for fold_i in range(config):
                 test_preds =  pd.read_csv(
-                        "../preds/{dataset}/{measure}/predictions_{dataset}_{i}_fold{fold_i}_{emb}.csv".format(dataset=dataset, i=method, fold_i=fold_i,
-                                                                                         emb=embedding, measure=measure.lower()))
+                        f"../preds/predictions_{dataset}_{method}_fold{fold_i}_{embedding}.csv")
 
                 val_preds =  pd.read_csv(
-                         "../preds/{dataset}/{measure}/predictions_{dataset}_{i}_fold{fold_i}_{emb}.csv".format(dataset=dataset, i=method, fold_i=fold_i, 
-                             emb=embedding, measure=measure.lower()))
-
-
+                    f"../preds/predictions_{dataset}_{method}_fold{fold_i}_{embedding}.csv")
 
                 scores_arr = eval(test_preds, scores_arr, int(len(test_preds)/2) , clf, embedding, fold_i, dataset)
                 val_scores_arr = eval(val_preds, val_scores_arr, int(len(val_preds) / 2), clf, embedding, fold_i, dataset)
 
-        pd.DataFrame(val_scores_arr).to_csv("../results/{dataset}/{measure}/val_experiments_{dataset}_{i}.csv".format(dataset=dataset, measure=measure.lower(), i=method))
+        pd.DataFrame(val_scores_arr).to_csv(f"../results/val_experiments_{dataset}_{method}.csv")
 
-        pd.DataFrame(scores_arr).to_csv("../results/{dataset}/{measure}/experiments_{dataset}_{i}.csv".format(dataset=dataset, i=method, measure=measure.lower()))
+        pd.DataFrame(scores_arr).to_csv(f"../results/experiments_{dataset}_{method}.csv")
     
     save_res('participatory', measure.lower(), dataset)
 
--- source/study2/umc_experiments.py
+++ source/study2/umc_experiments.py
@@ -1,36 +1,41 @@
-import collections
-from sklearn.model_selection import train_test_split
 from sklearn.model_selection import PredefinedSplit
-from sklearn.metrics import recall_score
-import nltk
-from aif360.metrics import ClassificationMetric
 from aif360.datasets import BinaryLabelDataset
-from aif360.algorithms.inprocessing.adversarial_debiasing import AdversarialDebiasing
-from sklearn.metrics import classification_report, make_scorer, mean_absolute_error, f1_score, confusion_matrix, \
-    roc_auc_score
+import collections
+from sklearn.model_selection import StratifiedKFold
+
 from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC, SVR
-import bias_analysis as bias
-from transformers import AutoTokenizer, AutoModel
 import numpy as np
-import gensim.downloader
-import gensim.downloader as api
-from sklearn.pipeline import make_pipeline, Pipeline
+from lime.lime_text import LimeTextExplainer
+from sklearn.pipeline import Pipeline
 import pandas as pd
-import sent2vec
-import tensorflow.compat.v1 as tf
-import re
-from aif360.algorithms.preprocessing.reweighing import Reweighing
 from aif360.algorithms.postprocessing.reject_option_classification\
         import RejectOptionClassification
-from aif360.algorithms.postprocessing.calibrated_eq_odds_postprocessing import CalibratedEqOddsPostprocessing
-from tqdm import tqdm
-from eval import eval
-tf.disable_eager_execution()
+
+from bias_mitigation import pre_processing
 
 privileged_groups = [{'GENDER': 1}]
 unprivileged_groups = [{'GENDER': 0}]
+
+## FEMALE: 0, MALE: 1
+pos = [140,140]
+neg = [1500,1500]
+
+val_pos = [12,60]
+val_neg = [265,160]
+
+svm_params = [{
+    'clf__C': [0.01, 0.1, 1, 10],
+    'clf__kernel': ['rbf'],
+}]
+
+C_values = [0.01, 0.1, 1, 10]
+embedding_length = collections.defaultdict(list)
+embedding_length['w2vec'] = 300
+embedding_length['biow2vec'] = 100
+embedding_length['bert'] = 768
+embedding_length['clinical_bert'] = 768
 
 
 def post_processing(val_aif360, test_aif360, val_data, test_data):
@@ -67,122 +72,11 @@ def post_processing(val_aif360, test_aif360, val_data, test_data):
     return val_data, test_data
 
 
-
-def pre_processing(aif360_train):
-    RW = Reweighing(unprivileged_groups=unprivileged_groups,
-                           privileged_groups=privileged_groups)
-    RW.fit(aif360_train)
-    dataset = RW.transform(aif360_train)
-    label_names = dataset.label_names  # or a list of label column names if available
-    sensitive_attribute_names = dataset.protected_attribute_names  # or a list of sensitive attribute column names if available
-
-    # Exclude label and sensitive attribute names from the feature names list
-    feature_names = [name for name in dataset.feature_names if name not in label_names + sensitive_attribute_names]
-    features_df = pd.DataFrame(dataset.features, columns=dataset.feature_names)
-    features_df = features_df[feature_names]
-
-    print(features_df.shape)
-    #features_df = pd.DataFrame(dataset.features, columns=[str(a) for a in range(embedding_length[embedding])])
-    return features_df, dataset.labels.ravel(), dataset.instance_weights
-
 def convert_dataset(train_data, clf):
-
     train_aif360 = BinaryLabelDataset(df=train_data, label_names=[clf], protected_attribute_names=['GENDER'])
-    
-
     # fav label:1, unfav: 0
     return  train_aif360
 
-
-def in_processing(train_aif360, test_aif360):
-    tf.reset_default_graph()
-    sess = tf.Session()
-    model = AdversarialDebiasing(privileged_groups=privileged_groups,
-                                 unprivileged_groups=unprivileged_groups,
-                                 # scope_name='plain_classifier',
-                                 scope_name='debiased_classifier',
-                                 # debias=False,
-                                 debias=True,
-                                 sess=sess,
-                                 batch_size=1,
-                                 num_epochs=100)
-
-    model.fit(train_aif360)
-    test_preds = model.predict(test_aif360)
-
-    classified_metric_debiasing_test = ClassificationMetric(test_aif360, test_preds,
-                                                            unprivileged_groups=unprivileged_groups,
-                                                            privileged_groups=privileged_groups)
-    print("Test set: Classification accuracy = %f" % classified_metric_debiasing_test.accuracy())
-    TPR = classified_metric_debiasing_test.true_positive_rate()
-    TNR = classified_metric_debiasing_test.true_negative_rate()
-    bal_acc_debiasing_test = 0.5 * (TPR + TNR)
-    print("Test set: Balanced classification accuracy = %f" % bal_acc_debiasing_test)
-    print("Test set: Disparate impact = %f" % classified_metric_debiasing_test.disparate_impact())
-    print(
-        "Test set: Equal opportunity difference = %f" % classified_metric_debiasing_test.equal_opportunity_difference())
-    print("Test set: Average odds difference = %f" % classified_metric_debiasing_test.average_odds_difference())
-    print("Test set: Theil_index = %f" % classified_metric_debiasing_test.theil_index())
-
-    return test_preds.labels
-
-## FEMALE: 0, MALE: 1
-import collections
-pos = [140,140]
-neg = [1500,1500]
-
-val_pos = [12,60]
-val_neg = [265,160]
-
-def folds_index(pos, neg, config):
-    folds = collections.defaultdict(list)
-    data = pd.read_csv("../UMC_w2vec_orig.csv")
-    np.random.seed(0)
-    clf = 'outcome'
-    folds[clf] = collections.defaultdict(list)
-
-    for i in range(config):
-        train_rows = []
-        test_rows = []
-        val_rows = []
-        for label, gender in [[1, 0], [1, 1], [0, 0], [0, 1]]:
-            sub = data[(data[clf] == label) & (data['GENDER'] == gender)].copy(deep=True)
-            if (label == 1):
-                no = pos[0] if gender == 0 else pos[1]
-                val_no = val_pos[0] if gender == 0 else val_pos[1]
-
-            else:
-                no = neg[0] if gender == 0 else neg[1]
-                val_no = val_neg[0] if gender == 0 else val_neg[1]
-
-
-            rows = np.random.choice(sub.index.values, no, replace=False)
-            #print("depression {d}, gender {g}, number of examples {e}".format(d=label, g=gender, e=len(rows)))
-            train_rows = np.concatenate([np.array(train_rows), rows]).astype(int)
-            rows2 = np.random.choice(sub[~sub.index.isin(rows)].index.values, val_no,
-                                     replace=False)
-            val_rows = np.concatenate([np.array(val_rows), rows2]).astype(int)
-            test_rows = np.concatenate(
-                [np.array(test_rows),
-                 sub[~sub.index.isin(np.concatenate([rows, rows2]))].index.values]).astype(int)
-
-
-        folds[clf][str(i)].append(train_rows)
-        folds[clf][str(i)].append(val_rows)
-        folds[clf][str(i)].append(test_rows)
-
-    return folds
-
-
-
-svm_params = [{
-    'clf__C': [0.01, 0.1, 1, 10],
-    'clf__kernel': ['rbf'],
-
-}]
-
-C_values = [0.01, 0.1, 1, 10]
-from sklearn.preprocessing import MinMaxScaler
 def classifier(X_train, X_val, y_train, y_val, weights, personalized=False):
 
     X_combined = pd.concat([X_train, pd.DataFrame(X_val)], ignore_index=True)
@@ -217,13 +111,7 @@ def get_predictions(model, X, y, index):
                               y_all], axis=1)
     return data
 
-# Commented out IPython magic to ensure Python compatibility.
-# %pip install lime
 
-from lime.lime_text import LimeTextExplainer
-from sklearn.pipeline import make_pipeline, Pipeline
-from sklearn.base import BaseEstimator
-from gensim.models.fasttext import load_facebook_model
 
 def get_vector(text, vectors):
     split_text = text.split(' ')
@@ -239,26 +127,6 @@ def get_vector(text, vectors):
         return None
     return np.average(np.asarray(vector_list), axis=0)
 
-#word_vectors = load_facebook_model('../cc.nl.300.bin')
-# class our_extractor(BaseEstimator):
-#
-#     # model = gensim.models.KeyedVectors.load_word2vec_format(
-#     #      "../resources/bio_embedding_extrinsic", binary=True)
-#     def __init__(self):
-#         print("... called... ")
-#
-#     def fit(X):
-#         return X
-#
-#     def transform(X):
-#         #print("Data in X: \n", X)
-#         if X:
-#             df = pd.DataFrame([get_vector(row, word_vectors) for row in X])
-#             return df
-#         else:
-#             return pd.DataFrame()
-#
-
 def explain(pipeline, index, X, method, clf):
     class_names = ['female', 'male']
     #print("index", index_num)
@@ -270,16 +138,9 @@ def explain(pipeline, index, X, method, clf):
 
     exp1 = explainer.explain_instance(text1
     , pipeline.predict_proba, num_features=50, labels=(1,))
-
     exp1.save_to_file('lime1_{index}_{method}_{clf}.html'.format(index=index, method=method, clf=clf))
 
-from sklearn.metrics import classification_report, make_scorer, mean_absolute_error, f1_score, confusion_matrix
 
-embedding_length = collections.defaultdict(list)
-embedding_length['w2vec'] = 300
-embedding_length['biow2vec'] = 100
-embedding_length['bert'] = 768
-embedding_length['clinical_bert'] = 768
 
 def swap_gender(gender):
     if gender == 0:
@@ -402,7 +263,6 @@ def get_gender_based_predictions(model1, model2, X_test, y_test):
 
     return data
 
-from sklearn.model_selection import StratifiedKFold
 def fold_cv(validation_split=0.1, config=5):
     validation_split = 0.1
     n_splits = 10
@@ -497,25 +357,21 @@ def experiment(method_name, folds, config, embedding, clf):
  
     return
 
-clf = 'outcome'
-#method_arr = ['orig', 'neutr', 'augmented']
-#method_arr = ['orig']
-embedding_arr = ['w2vec', 'biow2vec', 'bert', 'clinical_bert']
-#embedding_arr = ['biow2vec']
-#methods = {'orig': ['orig', 'postprocessing', 'gender_specific', 'preprocessing'], 'neutr': ['neutr'], 'augmented': ['augmented']}
-
-methods = {'orig': ['balanced']}
-attr = 'GENDER'
-clf = 'outcome'
-measure = 'f1'
 if __name__ == "__main__":
+    clf = 'outcome'
+    embedding_arr = ['w2vec', 'biow2vec', 'bert', 'clinical_bert']
+    # methods = {'orig': ['orig', 'postprocessing', 'gender_specific', 'preprocessing'], 'neutr': ['neutr'], 'augmented': ['augmented']}
+    
+    methods = {'orig': ['balanced']}
+    attr = 'GENDER'
+    clf = 'outcome'
+    measure = 'f1'
+    
     print("*********** Bias analysis EXPERIMENTS ********")
     config = 20
     for data_key, values in methods.items():
         for method in values:
             folds_ind = fold_cv(config=config)
-            #folds_ind = folds_index(pos, neg, config)
-            #print("folds_index", (folds_ind[str(0)]))
             for embedding in embedding_arr:
                 folds = create_fold_i(embedding, data_key, clf, folds_ind)
                 experiment(method, folds, config, embedding, clf)    
